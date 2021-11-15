8ed5f2d0a29718c1c18d5d3eb74bf4a221e52b94
Lân Bùi Tấn <tanlanhcmus@gmail.com>
2021-11-16 01:28:19 +0700

    update

--- app.js
+++ app.js
@@ -15,14 +15,6 @@ app.set('view engine', '.hbs');
 // const loginRoute = require('./routes/loginRoutes');
 // app.use('/login', loginRoute);
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('users/home');
 });
-app.get('/about-us', (req, res) => {
-    res.render('about-us')
-});
-app.get('/', middleware.requireLogin, (req, res) =>{
-    const payload = {
-        pageTitle: 'Home'
-    };
-    res.status(200).render('home', payload);
-});
+
