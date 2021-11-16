69ff179659b5b2333305a7d66e8e3dc5d55d79d2
Lân Bùi Tấn <tanlanhcmus@gmail.com>
2021-11-16 12:19:06 +0700

    add static file

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const exphbs  = require('express-handlebars');
 const app = express();
 const port = 3000;
+const path = require('path')
 const middleware = require('./middleware');
 const server = app.listen(port, () => console.log(`Server is listening on ${port}`));
 
@@ -14,6 +15,8 @@ app.set('view engine', '.hbs');
 // Router
 // const loginRoute = require('./routes/loginRoutes');
 // app.use('/login', loginRoute);
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/', (req, res) => {
     res.render('users/home');
 });
